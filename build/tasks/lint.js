820a86b1ceb219c6c219f9cb18d56b1b36dd3435
Erik Lieben <erik@codetistic.com>
2016-03-20 00:22:27 +0100

    chore(lint) add test dir to lint path

--- build/tasks/lint.js
+++ build/tasks/lint.js
@@ -1,9 +1,9 @@
 var gulp = require('gulp');
 var paths = require('../paths');
 var tslint = require('gulp-tslint');
- 
+
 gulp.task('lint', function() {
-  return gulp.src(paths.source)
+  return gulp.src([paths.source, 'test/unit/**/*.ts'])
     .pipe(tslint())
     .pipe(tslint.report('prose', {
       emitError: false
