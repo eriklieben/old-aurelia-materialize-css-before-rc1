8a8f932d897ab2dbc012193e8e9051233f971523
Erik Lieben <erik@codetistic.com>
2016-03-20 09:29:14 +0100

    test(config) add test cases for prefix & collapsible properties

--- test/unit/config.spec.ts
+++ test/unit/config.spec.ts
@@ -2,6 +2,26 @@ import {config} from "../../src/config";
 
 describe("the Aurelia Materialize CSS config", () => {
 
+    it("must have a property prefix with the default value 'm:'", () => {
+        expect(config.prefix).toBe("m:");
+    });
+
+    it("must have a property collapsible with the default value 'm:collapsible'", () => {
+        expect(config.collapsible).toBe("m:collapsible");
+    });
+
+    it("must have a property collapsibleBody with the default value 'm:collapsible-body'", () => {
+        expect(config.collapsibleBody).toBe("m:collapsible-body");
+    });
+
+    it("must have a property collapsibleHeader with the default value 'm:collapsible-header'", () => {
+        expect(config.collapsibleHeader).toBe("m:collapsible-header");
+    });
+
+    it("must have a property collapsibleItem with the default value 'm:collapsible-item'", () => {
+        expect(config.collapsibleItem).toBe("m:collapsible-item");
+    });
+
     it("must have a property dropdown with the default value 'm:dropdown'", () => {
         expect(config.dropdown).toBe("m:dropdown");
     });
