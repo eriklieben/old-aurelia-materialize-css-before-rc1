61216417152634bbd20bb38e9e4684564f23dc8d
Erik Lieben <erik@codetistic.com>
2016-03-28 11:13:35 +0200

    refactor(collapsible) make attribute and element work the same

--- src/javascript/collapsible/collapsibleBodyAttribute.ts
+++ src/javascript/collapsible/collapsibleBodyAttribute.ts
@@ -16,5 +16,4 @@ export class CollapsibleBodyAttribute {
     public detached() {
         this.element.classList.remove("collapsible-body");
     }
-
 }
--- src/javascript/collapsible/collapsibleElement.ts
+++ src/javascript/collapsible/collapsibleElement.ts
@@ -1,23 +1,44 @@
-import { customElement, containerless, bindable } from "aurelia-framework";
-import { bindingMode } from "aurelia-binding";
+import { bindable, customElement, containerless, inlineView } from "aurelia-framework";
 import { config } from "./../../config";
 
 @customElement(config.collapsible)
 @containerless
+@inlineView("<template><ul ref='element' id='${id}'><content></content></ul></template>")
 export class CollapsibleElement {
 
-    public collapsible;
+    public element: HTMLUListElement;
 
-    @bindable({ bindingMode: bindingMode.oneTime })
+    @bindable()
     public id: string;
 
-    @bindable({ bindingMode: bindingMode.oneTime, defaultValue: "accordion" })
+    @bindable({ defaultValue: "accordion" })
     public type: string;
 
-    @bindable({ bindingMode: bindingMode.oneTime, defaultValue: "" })
+    @bindable({ defaultValue: "" })
     public class: string;
 
     public attached() {
-        $(this.collapsible).collapsible();
+
+        this.element.classList.add("collapsible");
+        this.element.setAttribute("data-collapsible", this.type);
+
+        if (this.class !== undefined && this.class !== null) {
+            this.class.split(" ").forEach(element => {
+                if (element !== '') {
+                    this.element.classList.add(element);
+                }
+            });
+        }
+
+        $(this.element).collapsible();
+    }
+
+    public detached() {
+        this.element.removeAttribute("data-collapsible");
+
+        // There should not be any css class on the ul.
+        for(let item of this.element.classList) {
+            this.element.classList.remove(item);
+        }
     }
 }
